7550e2820f8c094fe7593655164c5e6395aabfe0
saf <41478485+safirex@users.noreply.github.com>
2022-12-05 11:59:48 +0100

    Add files via upload

---
+++ projet/projet/readFile.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+white = open("dataset/winequality-white.csv", "r")
+red = open("dataset/winequality-red.csv", "r")
+
+# get rid of csv header
+# white.readline()
+# red.readline()
+
+def get_column_labels():
+    column_names= ['fixed acidity','volatile acidity','citric acid','residual sugar','chlorides','free sulfur dioxide','total sulfur dioxide','density','ph','sulphates','alcohol','quality']
+    return column_names
+
+def read_red(n:int):
+    tab = []
+    for i in range(n+1):
+        tab.append(red.readline().replace('\n','').split(';'))
+    return pd.DataFrame(tab[1:],columns=get_column_labels()).astype('float') 
+
+def read_white(n:int):
+    tab = []
+    for i in range(n+1):
+        tab.append(white.readline().replace('\n','').split(';'))
+    return pd.DataFrame(tab[1:],columns=get_column_labels()).astype('float') 
